305a5e15424b48daaa87cb49983cd27c0b041981
zor07 <anzor.karmov@gmail.com>
2022-10-22 19:00:06 +0300

    renamed class

--- src/main/java/com/zor07/notesbackend/NotesApp.java
+++ src/main/java/com/zor07/notesbackend/NotesApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NotesBackendApplication {
+public class NotesApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(NotesBackendApplication.class, args);
+        SpringApplication.run(NotesApp.class, args);
     }
 
 }
