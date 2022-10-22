8f7d66ebacb8d551cf0d86cec8803e9e5abfb1f9
zor07 <anzor.karmov@gmail.com>
2022-10-22 17:25:02 +0300

    User entity

---
+++ src/main/java/com/zor07/notesbackend/entity/User.java
@@ -0,0 +1,94 @@
+package com.zor07.notesbackend.entity;
+
+import com.zor07.notesbackend.entity.Role;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Entity
+@Table(name = "user", schema = "public")
+public class User {
+  @Id
+  @GeneratedValue(
+      strategy = GenerationType.SEQUENCE,
+      generator = "user_id_seq"
+  )
+  @SequenceGenerator(
+      name = "user_id_seq",
+      sequenceName = "user_id_seq",
+      allocationSize = 1
+  )
+  private Long id;
+  private String name;
+  private String username;
+  private String password;
+
+  @ManyToMany(fetch = FetchType.EAGER)
+  @JoinTable(
+      name="user_roles",
+      joinColumns=@JoinColumn(name="user_id"),
+      inverseJoinColumns=@JoinColumn(name="role_id")
+  )
+  private Collection<Role> roles = new ArrayList<>();
+
+  public User() {
+  }
+
+  public User(Long id, String name, String username, String password, Collection<Role> roles) {
+    this.id = id;
+    this.name = name;
+    this.username = username;
+    this.password = password;
+    this.roles = roles;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public Collection<Role> getRoles() {
+    return roles;
+  }
+
+  public void setRoles(Collection<Role> roles) {
+    this.roles = roles;
+  }
+}
