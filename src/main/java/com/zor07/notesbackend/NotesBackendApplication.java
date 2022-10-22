cca6a7b4e3316187eb622667578a39fe4099fe2b
zor07 <anzor.karmov@gmail.com>
2022-10-22 16:14:33 +0300

    first commit

---
+++ src/main/java/com/zor07/notesbackend/NotesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.zor07.notesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotesBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zor07/notesbackend/NotesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zor07.notesbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
